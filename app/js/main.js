19a3a40a6b59e6dcc8d2bfe9f45b3102e3300f9d
AlexeySvistunov <xdayx53@gmail.com>
2023-07-05 14:24:03 +0300

    for pc =)

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function () {
+    $('.header__btn').on('click', function(){
+        $('.rightside-menu').removeClass('rightsize-menu--close');
+
+    });
+
+    $('.rightside-menu__close').on('click', function(){
+        $('.rightside-menu').addClass('rightsize-menu--close');
+
+    });
 
 })
